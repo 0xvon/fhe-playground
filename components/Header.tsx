edc0395c0e51a5b8bb62cff3e893ab04db6b674c
Masato Tsutsumi <masatotsutsumi@Sakos-Mac-Studio.local>
2024-02-27 09:40:48 +0900

    github icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
+import { FaGithub } from "react-icons/fa";
+
 const Header = () => {
     return (
-        <header className="bg-blue-500 text-white text-xl p-4">
-            FHE Playground
+        <header className="flex justify-between items-center p-4 bg-blue-500 text-white">
+            <h1 className="text-lg font-semibold">FHE Playground</h1>
+            <a href="https://github.com/0xvon/fhe-playground" target="_blank" rel="noopener noreferrer" className="flex items-center hover:text-gray-200">
+                <FaGithub className="mr-2" size={25} />
+            </a>
         </header>
     );
 };
